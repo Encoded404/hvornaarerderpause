8a153004c9142987db1c11e92fb979a8514da82b
Encoded <75113441+EncodedClass@users.noreply.github.com>
2024-09-03 10:51:15 +0200

    refector the code to use js scripting

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-forceShowTime = false;
-forceShowTimeTilNext = true;
+forceBreakNow = false;
+forceShowInboundTime = false;
+forceShowTimeTilNext = false;
 forceMiliseconds = false;
+
+//the time between each textupdate in millisecunds
+defaultUpdateInterval = 200;
 
 function getTime() {
     var today = new Date();
@@ -20,17 +24,18 @@ function getTime() {
     let seconds = Math.floor(miliseconds / 1000);
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
+    let days = Math.floor(hours / 24);
     
     hours = hours - days * 24;
     minutes = minutes - days * 24 * 60 - hours * 60;
     seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
     miliseconds = miliseconds - days * 24 * 60 * 60 * 1000 - hours * 60 * 60 * 1000 - minutes * 60 * 1000 - seconds * 1000;
     
-    // Add a leading zero if the number is less than 10
-    if(seconds < 10) { seconds = "0" + seconds;}
+    // Add a leading zero if the number is less than 100
+    if(miliseconds < 100) { miliseconds = "0" + miliseconds;}
+    if(miliseconds < 10) { miliseconds = "0" + miliseconds;}
 
-    console.log(seconds)
+    //console.log(seconds)
 
     return {
         miliseconds: miliseconds,
@@ -41,123 +46,131 @@ function getTime() {
     };
 }
 
+function isEven(n) {
+    return n % 2 == 0;
+ }
+ 
+ function isOdd(n) {
+    return Math.abs(n % 2) == 1;
+ }
+
+daysMessage = "dage"
+hoursMessage = "timer"
+minutesMessage = "minutter"
+secondsMessage = "sekunder"
+millisecondsMessage = "millisekunder"
+
+breakIsLongInboundMessage = "der er først coding pirates igen om:"
+breakIsInboundMessage = "der er pause om:"
+breakIsOverMessage = "pausen er ovre nu..."
+breakMessage = "nu er der pause!"
+
+numberContainerText = "<span class=\"clockNumber\">"
+blinkContainerText = "<span class=\"blink\">"
+numberContainerTextEnd = "</span>"
+
 function updateTime() {
     // Get the time left
     let t = getTime();
 
-    console.log(t.days.toString() + " " + t.hours.toString() + " " + t.minutes.toString());
+    let finalMessage = "";
+    let shouldAddComma = false;
 
-    // Update the time left
-    pauseNow.classList.add("hidden")
-    pauseIncomming.classList.add("hidden")
-    notAnymoreToday.classList.add("hidden")
-    thatsItForNow.classList.add("hidden")
-    preciseTimer.classList.add("hidden")
-    let updateInterval = 100;
-    if (forceShowTime && !forceShowTimeTilNext) {
-        pauseIncomming.classList.remove("hidden")
-        minutes.innerHTML = t.minutes
-        seconds.innerHTML = t.seconds
-        if(t.seconds < 10 || forceMiliseconds)
+    updateInterval = defaultUpdateInterval;
+
+    //check if there is more than 1 hour til the break
+    if((t.days > 0 || hours > 1) 
+        && !forceShowInboundTime && !forceBreakNow || forceShowTimeTilNext) //check debug variables
+    {
+        clockText.innerHTML = breakIsLongInboundMessage;
+
+        if(forceMiliseconds)
         {
-            updateInterval = 1;
-            preciseTimer.classList.remove("hidden");
-            preciseTimer.innerHTML = "." + Math.floor(t.miliseconds/10);
+            finalMessage = finalMessage + numberContainerText + t.miliseconds + numberContainerTextEnd+ " " + millisecondsMessage
+            shouldAddComma = true;
+            updateInterval = 0;
         }
-    }
-    else if (t.hours == 23 && t.days == -1 && t.minutes > 49 && !forceShowTimeTilNext) {
-        pauseNow.classList.remove("hidden");
-    } else if(t.days != -1 || forceShowTimeTilNext) { 
-        thatsItForNow.classList.remove("hidden");
-
-        NextTimeDays.classList.add("hidden");
-        NextTimeHours.classList.add("hidden");
-        NextTimeMinutes.classList.add("hidden");
-        NextTimeSeconds.classList.add("hidden");
-
-        
-        NextTimeDaysContainer.classList.add("hidden");
-        NextTimeHoursContainer.classList.add("hidden");
-        NextTimeMinutesContainer.classList.add("hidden");
-        NextTimeSecondsContainer.classList.add("hidden");
-
-        commaBeforeHours.classList.add("hidden");
-        commaBeforeMinutes.classList.add("hidden");
-        commaBeforeSeconds.classList.add("hidden");
-
-        commaBeforeMinutes.classList.add("hidden");
-
-
-        
-        if(t.days != 0)
-        {
-            NextTimeDays.classList.remove("hidden");
-            NextTimeDaysContainer.classList.remove("hidden");
-
-            NextTimeDays.innerHTML = t.days;
-        }
-        if(t.hours != 0)
-        {
-            commaBeforeHours.classList.remove("hidden");
-            NextTimeHours.classList.remove("hidden");
-            NextTimeHoursContainer.classList.remove("hidden");
-
-            NextTimeHours.innerHTML = t.hours;
-        }
-        if(t.minutes != 0)
-        {
-            commaBeforeMinutes.classList.remove("hidden");
-            NextTimeMinutes.classList.remove("hidden");
-            NextTimeMinutesContainer.classList.remove("hidden");
-
-            NextTimeMinutes.innerHTML = t.minutes;
-        }
+    
         if(t.seconds != 0)
         {
-            commaBeforeSeconds.classList.remove("hidden");
-            NextTimeSeconds.classList.remove("hidden");
-            NextTimeSecondsContainer.classList.remove("hidden");
-
-            NextTimeSeconds.innerHTML = t.seconds;
+            let addComma = ""
+            if (shouldAddComma) {addComma = ", <br>"}
+            finalMessage = numberContainerText + t.seconds + numberContainerTextEnd + " " + secondsMessage + addComma + finalMessage;
+            shouldAddComma = true;
         }
-    } else {
-        notAnymoreToday.classList.remove("hidden")
-    }
-    if (t.hours < 24 && t.days == 0) {
-        pauseIncomming.classList.remove("hidden")
-        minutes.innerHTML = t.minutes
-        seconds.innerHTML = t.seconds
-        if(t.seconds < 10 || forceMiliseconds)
+    
+        if(t.minutes != 0)
         {
-            updateInterval = 1;
-            preciseTimer.classList.remove("hidden");
-            preciseTimer.innerHTML = "." + Math.floor(t.miliseconds/10);
+            let addComma = ""
+            if (shouldAddComma) {addComma = ", <br>"}
+            finalMessage = numberContainerText + t.minutes + numberContainerTextEnd + " " + minutesMessage + addComma + finalMessage;
+            shouldAddComma = true;
+        }
+    
+        if(t.hours != 0)
+        {
+            let addComma = ""
+            if (shouldAddComma) {addComma = ", <br>"}
+            finalMessage = numberContainerText + t.hours + numberContainerTextEnd + " " + hoursMessage + addComma + finalMessage;
+            shouldAddComma = true;
+        }
+    
+        if(t.days != 0)
+        {
+            let addComma = ""
+            if (shouldAddComma) {addComma = ", <br>"}
+            finalMessage = numberContainerText + t.days + numberContainerTextEnd + " " + daysMessage + addComma + finalMessage;
+            shouldAddComma = true;
         }
     }
-    setTimeout(updateTime, updateInterval)
+    //check if it's less than 10 minutes since the break started. display breakMessage if true
+    else if (t.hours == 23 && t.days == -1 && t.minutes > 49
+         && !forceShowTimeTilNext && !forceShowInboundTime || forceBreakNow) //check debug variables
+    {
+        clockText.innerHTML = breakMessage;
+    }
+    //check if theres less than an hour til the break
+    else if(t.hours <= 1 
+        || forceShowInboundTime) //check debug variables
+    {
+        
+        clockText.innerHTML = breakIsInboundMessage;
+        if(forceMiliseconds || (t.seconds < 10 && t.minutes == 0 && t.hours == 0 && t.days == 0))
+        {
+            finalMessage = finalMessage + numberContainerText + t.miliseconds + numberContainerTextEnd
+            shouldAddComma = true;
+            updateInterval = 0;
+        }
+        
+        if(t.seconds < 10) {t.seconds = "0" + t.seconds}
+        let addComma = ""
+        if (shouldAddComma) {addComma = "."}
+        finalMessage = numberContainerText + t.seconds + numberContainerTextEnd + addComma + finalMessage;
+        shouldAddComma = true;
+
+        let showBlink = isEven(t.seconds)
+        let blinkMessage = ":"
+        if(!showBlink) {blinkMessage = " "}
+        
+        if(t.minutes < 10) {t.minutes = "0" + t.minutes}
+        finalMessage = numberContainerText + t.minutes + numberContainerTextEnd + blinkContainerText + blinkMessage + numberContainerTextEnd + finalMessage;
+        shouldAddComma = true;
+
+        if(t.hours != 0)
+        {
+            finalMessage = numberContainerText + t.hours + numberContainerTextEnd + blinkContainerText + blinkMessage + numberContainerTextEnd + finalMessage;
+            shouldAddComma = true;
+        }
+    }
+
+    console.log(finalMessage)
+    clockContainer.innerHTML = finalMessage;
+    setTimeout(updateTime, updateInterval);
 }
 
+clockText = document.getElementsByClassName("clockText")[0]
 
-commaBeforeHours = document.querySelector('#commaBeforeHours');
-commaBeforeMinutes = document.querySelector('#commaBeforeMinutes');
-commaBeforeSeconds = document.querySelector('#commaBeforeSeconds');
-
-NextTimeDays = document.querySelector('#NextTimeDays');
-NextTimeHours = document.querySelector('#NextTimeHours');
-NextTimeMinutes = document.querySelector('#NextTimeMinutes');
-NextTimeSeconds = document.querySelector('#NextTimeSeconds');
-
-NextTimeDaysContainer = document.querySelector('#NextTimeDaysContainer');
-NextTimeHoursContainer = document.querySelector('#NextTimeHoursContainer');
-NextTimeMinutesContainer = document.querySelector('#NextTimeMinutesContainer');
-NextTimeSecondsContainer = document.querySelector('#NextTimeSecondsContainer');
-
-minutes = document.querySelector('#minutes');
-seconds = document.querySelector('#seconds');
-pauseNow = document.getElementById("pauseNow");
-pauseIncomming = document.getElementById("pauseIncomming");
-preciseTimer = document.getElementById("preciseTimer");
-notAnymoreToday = document.getElementById("notAnymoreToday");
-thatsItForNow = document.getElementById("thatsItForNow");
+clockHeader = document.getElementById("clockHeader");
+clockContainer = document.getElementById("nextTimeClock");
 
 updateTime()
